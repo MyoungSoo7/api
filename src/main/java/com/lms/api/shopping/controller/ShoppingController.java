6238f1f01c9c23926f807ddb98fd11a2c2242f68
fastcamedu <iamipro@naver.com>
2023-12-12 02:42:24 +0900

    api 구조변경

--- src/main/java/com/lms/api/shopping/controller/ShoppingController.java
+++ src/main/java/com/lms/api/shopping/controller/ShoppingController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.internal.StringUtil;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,11 +15,12 @@ import java.util.Objects;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api")
 public class ShoppingController {
 
     private final NaverShoppingService naverShoppingService;
 
-    @GetMapping("/api/shopping-item")
+    @GetMapping(path="/shopping")
     @ResponseBody
     public List<NaverShoppingSearchResultDto> getShoppingItems(@RequestParam(value="query") String query) throws IOException {
 
@@ -29,10 +28,9 @@ public class ShoppingController {
             query = "커피";
         }
         List<NaverShoppingSearchResultDto> naverShoppingSearchResultDtoList = naverShoppingService.getItems(query);
-        for(NaverShoppingSearchResultDto naverShoppingSearchResultDto : naverShoppingSearchResultDtoList){
-            log.info("itemDto=>"+ naverShoppingSearchResultDto.toString());
-        }
         return naverShoppingSearchResultDtoList;
     }
+
+
 
 }
